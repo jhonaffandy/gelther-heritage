6efb8845928e3ab07a55774c89c4b375977da11f
Johan <joe.affandy@gmail.com>
2024-08-18 17:15:28 +0700

    feat:gelther-heritage-init
    create new repository -tutorial bwa

---
+++ app/Models/Transaction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaction extends Model
+{
+ 
+    use HasFactory,SoftDeletes;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'users_id',
+        'address',
+        'total_price',
+        'shipping_price',
+        'payment',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+
+    public function itemss()
+    {
+        return $this->hasMany(TransactionItem::class,'transactions_id','id');
+    }
+}
---
+++ app/Models/TransactionItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class TransactionItem extends Model
+{
+
+    use HasFactory,SoftDeletes;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'users_id',
+        'products_id',
+        'transactions_id',
+        'quantity'
+    ];
+
+    public function product()
+    {
+        return $this->hasOne(Product::class,'id','product_id');
+    }
+}
