04b5b4e9f2398fdf20f14f06e0a93fe6fb1603ea
Johan <joe.affandy@gmail.com>
2024-08-18 19:47:08 +0700

    finish:API Kategori
    ProductCategoryController
    ProductController -> fixing some point

--- app/Http/Controllers/API/ProductCategoryController.php
+++ app/Http/Controllers/API/ProductCategoryController.php
@@ -2,10 +2,50 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 
 class ProductCategoryController extends Controller
 {
-    //
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit');
+        $name = $request->input('name');
+        $show_product = $request->input('show_product');
+
+        if ($id) {
+            $category = ProductCategory::with(['products'])->find($id);
+
+            if ($category) {
+                return ResponseFormatter::success(
+                    $category,
+                    'Data kategori berhasil diambil'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data kategoti tidak ada',
+                    404
+                );
+            }
+        }
+
+        $category = ProductCategory::query();
+
+        if ($name) {
+            $category->where('name', 'LIKE', '%' . $name . '%');
+        }
+
+        if ($show_product) {
+            $category->with('products');
+        }
+
+        return ResponseFormatter::success(
+            $category->paginate($limit),
+            'Data kategori berhasil diambil'
+        );
+    }
 }
